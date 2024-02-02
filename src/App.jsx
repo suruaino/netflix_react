5e5b0032660ad09fa6fcf69baabf181fc7083c24
SURU EMMANUEL <suruaino@gmail.com>
2024-02-02 23:56:48 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+
+
+const Header = () => {
+
+  return(
+    <header className='main_header'>
+      <div className="logo">NETFLIX</div>
+      <a href='#'>SIGN IN</a>
+    </header>
+
+  )
+}
+
+const Hero =() => {
+
+  return(
+    <>
+      
+    </>
+  )
+}
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  )
+}
+
+export default App
